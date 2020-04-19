b243adf872805dee36118e6d14471933f9f9de33
Dieva Leon <58702512+dievaGit@users.noreply.github.com>
2020-04-18 23:44:06 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //receiving special character
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            //building the triangle
+            Console.WriteLine("     ©    \n    © ©   \n   ©   ©  \n  ©     © \n © © © © ©");
+
+            Console.ReadKey();
+        }
+    }
+}
